1fd45eccf705685740e297af85447e1c67950730
Michal J. Gajda <mjgajda@gmail.com>
2015-04-22 01:42:24 +0800

    Cleaned up version.

--- DigitalClock.hs
+++ DigitalClock.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE QuasiQuotes                #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
+-- | This module implements a simple digital stopwatch.
 module Main where
 
 import           Prelude                 () -- no implicit Prelude!
@@ -18,29 +19,26 @@ import           CLaSH.Signal
 import           CLaSH.Signal.Explicit
 import           CLaSH.Signal.Bundle
 
--- | Clock implicit
-every :: (Num t, Eq t) => t -> Unbundled Bool -> Unbundled Bool
-every x = fsm <^> 0
-  where
-    fsm _  True          = (0,    False)
-    fsm st _   | x == st = (0,    True )
-    fsm st _             = (st+1, False)
-
 type Word17 = Unsigned 17
 
+-- | Gives a signal once every second, for the duration of a single System Clock cycle.
 everySecond :: Signal Bool -- ^ trigger every one second
 everySecond  = counter' fpgaFrequency
 -- $ (2 :: Word17) ^ (15 :: Word17) * (1000 :: Word17) -- or Signed 16?
 
--- using 32 bit arithmetic is a bit lazy
+-- | FPGA frequency for my Papillio 250K is 32.768MHz.
+-- 
+-- Here, I am using 32 bit arithmetic to count second,
+-- instead of splitting it into smaller counters and connecting them.
 fpgaFrequency :: Unsigned 27
---fpgaFrequency  = 1000 -- reduced for debugging
 fpgaFrequency  = 32768000 -- real 32MHz
 
--- TODO: These have extra clocks!
+-- | Counter that cycles every time it gets a given @True@ signal at the input,
+-- and itself gives the current count, and also the @True@ signal only
+-- when the limit is reached.
 counter      :: (Num s, Eq s) =>
                  s            -> -- ^ number of clock cycles before overflow and reset
-                 Signal Bool  -> -- ^ trigger for clock cycle (not the clock domain!)
+                 Signal Bool  -> -- ^ input trigger for the clock cycle (not the clock domain!)
                  Unbundled (s, Bool)
 counter limit = fsm <^> 0
   where
@@ -48,6 +46,8 @@ counter limit = fsm <^> 0
     fsm st True | limit == st+1 = (0,    (0,    True ))
     fsm st True                 = (st+1, (st+1, False))
 
+-- | Simple counter without any inputs.
+-- Gives true signal only when the given limit is reached.
 counter'      :: (Num s, Eq s) =>
                   s            -> -- ^ number of clock cycles before overflow and reset
                   Signal Bool
@@ -98,9 +98,9 @@ sevenSegmentDigit 13 = [b| 1001110 |] -- empty
 sevenSegmentDigit 14 = [b| 0000110 |] -- empty
 sevenSegmentDigit 15 = [b| 0001110 |] -- empty
 
-
+-- | 1kHz clock for changing seven segment anode without flicker
 hz1000 :: Signal Bool
-hz1000 = counter' 32768 --(1 :: Unsigned 2) -- (32768 :: Unsigned 17)
+hz1000 = counter' 32768
 -- ((2^15) :: Word17)
 
 -- | Interface to seven segment display.
@@ -112,12 +112,14 @@ data SevenSegmentDisplay n = SevenSegmentDisplay {
 
 instance (KnownNat n) => Bundle (SevenSegmentDisplay n) where
   type Unbundled' t (SevenSegmentDisplay n) = (Signal' t (Unsigned n),
-                                               Signal' t (SevenSegDigit))
+                                               Signal' t  SevenSegDigit)
   unbundle' _  s                                = (anodeIndex <$> s, currentDigit <$> s)
   bundle'   _ (anode,                    digit) = SevenSegmentDisplay <$> anode <*> digit
 
--- This seems to make compiler loop!
---digitAnode i = 2^i
+
+-- | Anode states for a given digit to display.
+-- NOTE: Generic version seems to make compiler loop as of 0.5/0.5.2.
+-- digitAnode i = 2^i
 digitAnode  :: (KnownNat n) => Unsigned n -> Unsigned n
 digitAnode 0 = [b|0111|]
 digitAnode 1 = [b|1011|]
@@ -143,10 +145,13 @@ sevenSegmentDisplay digits = bundle (digitAnode        <$> whichDigit  ,
 topEntity ::  Signal (SevenSegmentDisplay 4)
 topEntity  = sevenSegmentDisplay hourClock 
 
+-- * Here are helpers for simulation.
+-- | Takes every nth step of simulated signal.
 takeEvery :: Int -> [a] -> [a]
 takeEvery n = go
   where
     go []     = []
     go (b:bs) = b:go (List.drop (fromIntegral n) bs)
 
+main :: IO ()
 main = print $ takeEvery ((fromIntegral fpgaFrequency `div` 10)) $ sampleN (10*fromIntegral fpgaFrequency) topEntity 
